45dd1dc911668c02967981ec23540252b3c74ab4
muddybootscode <firsttogo98@gmail.com>
2020-08-18 09:54:05 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,138 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import initialData from "./initialData";
+import Column from "./Column";
+import { DragDropContext } from "react-beautiful-dnd";
+import {gql, useMutation, useQuery } from "@apollo/client";
+
+const GET_TABLES = gql`
+  query GetTables($title: String){
+    Table(title: $title){
+        id
+        title
+        columnOrder
+        columns{
+            id
+            title
+            taskIds
+            tasks{
+                id
+                content
+            }
+        }
+    }
+  }
+`
+
+const COL_UPDATE = gql`
+  mutation UpdateColumn($id: ID!, $title: String, $taskIds: [ID], $tasks: [Task]){
+    UpdateColumn(id: $id, title: $title, taskIds: $taskIds, tasks: $tasks){
+        id,
+        title,
+        taskIds,
+        tasks,
+    }
+  }
+`
 
 function App() {
+  const [state, setState] = useState(initialData);
+  const { loading, error, data } = useQuery(GET_TABLES, {variables: 'Test Table'});
+  const [update, { updateLoading, updateError}] = useMutation(COL_UPDATE)
+
+  const onDragEnd = (result) => {
+    const {destination, source, draggableId} = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = state.columns[source.droppableId];
+    const newTaskIds = [...column.taskIds]
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds
+    };
+
+    const newState = {
+      ...state,
+      columns: {
+        ...state.columns,
+        [newColumn.id]: newColumn
+      }
+    };
+
+    setState(newState);
+    console.log(state)
+
+  };
+
+  const setTable = (data) => {
+    const { Table } = data;
+    const tasks = {};
+    const columns = {};
+    const columnOrder = Table[0].columnOrder;
+    // Pull all tasks out into their own object
+    Table[0].columns.forEach((col) => {
+      col.tasks.forEach((task) => {
+        tasks[task.id] = {id: task.id, content: task.content}
+      })
+    });
+    // Pull out all columns and their associated task ids
+    Table[0].columns.forEach((col) => {
+      columns[col.id] = {id: col.id, title: col.title, taskIds: col.taskIds}
+    })
+
+    const table = {
+      tasks,
+      columns,
+      columnOrder
+    }
+    setState(table)
+  }
+
+  useEffect(() => {
+    console.log(state, ' From State Change')
+  }, [state])
+
+  useEffect(() => {
+
+    if(data) {
+      setTable(data)
+    }
+  }, [data])
+
+  if (loading) {
+    return <div>...Loading</div>
+  }
+
+  if (error) {
+    console.warn(error)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DragDropContext
+        onDragEnd={onDragEnd}
+      >
+        {
+          state.columnOrder.map((columnId) => {
+            const column = state.columns[columnId];
+            const tasks = column.taskIds.map(taskId => state.tasks[taskId])
+            return <Column key={column.id} column={column} tasks={tasks}/>
+          })
+        }
+      </DragDropContext>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import '@atlaskit/css-reset';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_GRAPHQL_URI || 'http://localhost:4001/graphql',
+  cache: new InMemoryCache(),
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
