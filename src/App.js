24c48166d3bcd7ff23b0d292ebdd8b5c339e72be
muddybootscode <firsttogo98@gmail.com>
2020-08-19 07:29:05 -0400

    Feature: Columns update correctly as well as graph correctly reflects the state of the table

--- src/App.js
+++ src/App.js
@@ -1,38 +1,56 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import initialData from "./initialData";
 import Column from "./Column";
-import { DragDropContext } from "react-beautiful-dnd";
-import { gql, useMutation, useQuery } from "@apollo/client";
+import {DragDropContext} from "react-beautiful-dnd";
+import {gql, useMutation, useQuery} from "@apollo/client";
 import styled from "styled-components";
 
 const GET_TABLES = gql`
-  query GetTables($title: String){
-    Table(title: $title){
-        id
-        title
-        columnOrder
-        columns{
+    query GetTables($title: String){
+        Table(title: $title){
             id
             title
-            taskIds
-            tasks{
+            columnOrder
+            columns{
                 id
-                content
+                title
+                taskIds
+                tasks{
+                    id
+                    content
+                }
             }
         }
     }
-  }
 `
 
 const COL_UPDATE = gql`
-  mutation UpdateColumn($id: ID!, $title: String, $taskIds: [ID]){
-    UpdateColumn(id: $id, title: $title, taskIds: $taskIds){
-        id,
-        title,
-        taskIds,
+    mutation UpdateColumn($id: ID!, $title: String, $taskIds: [ID]){
+        UpdateColumn(id: $id, title: $title, taskIds: $taskIds){
+            id
+        }
     }
-  }
+`
+
+const REMOVE_TASK = gql`
+    mutation RemoveTaskColumn($from: _TaskInput!, $to: _ColumnInput!){
+        RemoveTaskColumn(from: $from, to: $to){
+            to {
+                id
+            }
+        }
+    }
+`
+
+const ADD_TASK = gql`
+    mutation AddTaskColumn($from: _TaskInput!, $to: _ColumnInput!){
+        AddTaskColumn(from: $from, to: $to){
+            to {
+                id
+            }
+        }
+    }
 `
 
 const Container = styled.div`
@@ -41,8 +59,10 @@ const Container = styled.div`
 
 function App() {
   const [state, setState] = useState(initialData);
-  // const { loading, error, data } = useQuery(GET_TABLES, {variables: 'Test Table'});
-  // const [colUpdate] = useMutation(COL_UPDATE)
+  const {loading, error, data} = useQuery(GET_TABLES, {variables: 'Test Table'});
+  const [colUpdate] = useMutation(COL_UPDATE)
+  const [addTask] = useMutation(ADD_TASK);
+  const [removeTask] = useMutation(REMOVE_TASK);
 
   const onDragEnd = (result) => {
     const {destination, source, draggableId} = result;
@@ -60,7 +80,7 @@ function App() {
 
     const start = state.columns[source.droppableId];
     const finish = state.columns[destination.droppableId]
-    if (start === finish){
+    if (start === finish) {
       const newTaskIds = [...start.taskIds]
       newTaskIds.splice(source.index, 1);
       newTaskIds.splice(destination.index, 0, draggableId);
@@ -79,14 +99,17 @@ function App() {
       };
 
       setState(newState);
-      // colUpdate({variables: {
-      //   ...newColumn
-      //   }})
-      //   .catch(error => console.log(error))
+      colUpdate({
+        variables: {
+          ...newColumn
+        }
+      })
+        .catch(error => console.log(error))
       return;
     }
 
     const startTaskIds = [...start.taskIds];
+    const taskId = startTaskIds[source.index];
     startTaskIds.splice(source.index, 1);
     const newStart = {
       ...start,
@@ -110,51 +133,84 @@ function App() {
     }
 
     setState(newState)
+    colUpdate({
+      variables: {
+        ...newStart
+      }
+    })
+      .then((data) => {
+        const {data: {UpdateColumn: {id}}} = data;
+        removeTask({
+          variables: {
+            from: {id: taskId},
+            to: {id}
+          }
+        })
+          .catch(error => console.log(error))
+      })
+      .catch(error => console.log(error))
+
+    colUpdate({
+      variables: {
+        ...newFinish
+      }
+    })
+      .then((data) => {
+        const {data: {UpdateColumn: {id}}} = data;
+        addTask({
+          variables: {
+            from: {id: taskId},
+            to: {id}
+          }
+        })
+          .catch(error => console.log(error))
+      })
+      .catch(error => console.log(error))
 
   };
 
-  // const setTable = (data) => {
-  //   const { Table } = data;
-  //   const tasks = {};
-  //   const columns = {};
-  //   const columnOrder = Table[0].columnOrder;
-  //   // Pull all tasks out into their own object
-  //   Table[0].columns.forEach((col) => {
-  //     col.tasks.forEach((task) => {
-  //       tasks[task.id] = {id: task.id, content: task.content}
-  //     })
-  //   });
-  //   // Pull out all columns and their associated task ids
-  //   Table[0].columns.forEach((col) => {
-  //     columns[col.id] = {id: col.id, title: col.title, taskIds: col.taskIds}
-  //   })
-  //
-  //   const table = {
-  //     tasks,
-  //     columns,
-  //     columnOrder
-  //   }
-  //
-  //   setState(table)
-  // }
+  const setTable = (data) => {
+    const {Table} = data;
+    const tasks = {};
+    const columns = {};
+    const columnOrder = Table[0].columnOrder;
+    // Pull all tasks out into their own object
+    Table[0].columns.forEach((col) => {
+      col.tasks.forEach((task) => {
+        tasks[task.id] = {id: task.id, content: task.content}
+      })
+    });
+    // Pull out all columns and their associated task ids
+    Table[0].columns.forEach((col) => {
+      columns[col.id] = {id: col.id, title: col.title, taskIds: col.taskIds}
+    })
 
-  // useEffect(() => {
-  //
-  //   if(data) {
-  //     setTable(data)
-  //   }
-  // }, [data])
-  //
-  // if (loading) {
-  //   return <div>...Loading</div>
-  // }
-  //
-  // if (error) {
-  //   console.warn(error)
-  // }
+    const table = {
+      tasks,
+      columns,
+      columnOrder
+    }
+
+    setState(table)
+  }
+
+  useEffect(() => {
+
+    if (data) {
+      setTable(data)
+    }
+  }, [data])
+
+  if (loading) {
+    return <div>...Loading</div>
+  }
+
+  if (error) {
+    console.warn(error)
+  }
 
   return (
-    <div className="App">
+    <div className="App" style={{ display: 'flex', justifyContent: 'space-around'}}>
       <DragDropContext
         onDragEnd={onDragEnd}
       >
