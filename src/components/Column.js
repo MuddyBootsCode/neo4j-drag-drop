ba42f03980708a69b60ec2b0509272085943e198
muddybootscode <firsttogo98@gmail.com>
2020-09-15 17:32:44 -0400

    Feature: Added Prettier

--- src/components/Column.js
+++ src/components/Column.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import Paper from "@material-ui/core/Paper";
-import {makeStyles} from "@material-ui/core/styles";
-import {Droppable} from "react-beautiful-dnd";
-import styled from "styled-components";
-import Task from './Task'
-import {Divider} from "@material-ui/core";
-import {gql} from "@apollo/client";
-import Fab from "@material-ui/core/Fab";
-import Tooltip from "@material-ui/core/Tooltip";
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import { Droppable } from 'react-beautiful-dnd';
+import styled from 'styled-components';
+import Task from './Task';
+import { Divider } from '@material-ui/core';
+import Fab from '@material-ui/core/Fab';
+import Tooltip from '@material-ui/core/Tooltip';
 import AddIcon from '@material-ui/icons/Add';
 
 const useStyles = makeStyles((theme) => ({
@@ -16,55 +15,52 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column',
     width: '300px',
     margin: '1em',
-    backgroundColor: 'lightgrey'
+    backgroundColor: 'lightgrey',
   },
   title: {
     padding: '8px',
     display: 'flex',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
   },
   taskList: {
     padding: '8px',
     transition: 'background-color 0.2s ease',
-    backgroundColor: (props) => props.isDraggingOver ? 'skyblue' : 'grey',
+    backgroundColor: (props) => (props.isDraggingOver ? 'skyblue' : 'grey'),
     flexGrow: 1,
-    minHeight: '100px'
-  }
+    minHeight: '100px',
+  },
 }));
 
 const TaskList = styled.div`
   padding: 8px;
   transition: background-color 0.2s ease;
-  background-color: ${props => (props.isDraggingOver ? 'skyblue' : 'white')};
+  background-color: ${(props) => (props.isDraggingOver ? 'skyblue' : 'white')};
   flex-grow: 1;
   min-height: 100px;
 `;
 
-const Column = ({column, tasks, addTask}) => {
+const Column = ({ column, tasks, addTask }) => {
   const classes = useStyles();
   return (
     <Paper className={classes.paper}>
       <div className={classes.title}>
         <h3>{column.title}</h3>
-        <Fab color="primary" size="small" onClick={() => addTask(column.id)}>
-          <Tooltip title="Add Task">
-            <AddIcon/>
+        <Fab color='primary' size='small' onClick={() => addTask(column.id)}>
+          <Tooltip title='Add Task'>
+            <AddIcon />
           </Tooltip>
         </Fab>
-
       </div>
-      <Divider/>
+      <Divider />
       <Droppable droppableId={column.id}>
-        {(provided, snapshot) =>
-          <TaskList
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-            isDraggingOver={snapshot.isDraggingOver}
-          >
-            {tasks.map((task, index) => <Task key={task.id} task={task} index={index}/>)}
+        {(provided, snapshot) => (
+          <TaskList ref={provided.innerRef} {...provided.droppableProps} isDraggingOver={snapshot.isDraggingOver}>
+            {tasks.map((task, index) => (
+              <Task key={task.id} task={task} index={index} />
+            ))}
             {provided.placeholder}
           </TaskList>
-        }
+        )}
       </Droppable>
     </Paper>
   );
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,117 +1,114 @@
-import React, {useEffect, useState} from 'react';
-import {Container} from "@material-ui/core";
-import {makeStyles} from '@material-ui/core/styles';
-import initialData from "../initialData";
-import Column from "../components/Column";
-import { DragDropContext } from "react-beautiful-dnd";
-import Paper from "@material-ui/core/Paper";
-import {useMutation, useQuery} from "@apollo/client";
-import {GET_TABLE, COL_UPDATE, ADD_TASK, REMOVE_TASK, CREATE_TASK} from "../queries/tableQueries";
+import React, { useEffect, useState } from 'react';
+import { Container } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import initialData from '../initialData';
+import Column from '../components/Column';
+import { DragDropContext } from 'react-beautiful-dnd';
+import Paper from '@material-ui/core/Paper';
+import { useMutation, useQuery } from '@apollo/client';
+import { GET_TABLE, COL_UPDATE, ADD_TASK, REMOVE_TASK, CREATE_TASK } from '../queries/tableQueries';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-      justifyContent: 'space-around',
-      height: '100vh',
-      backgroundColor: 'lightblue'
-    },
-    paper: {
-      margin: '1em',
-      display: 'flex'
-    }
+  root: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    height: '100vh',
+    backgroundColor: 'lightblue',
+  },
+  paper: {
+    margin: '1em',
+    display: 'flex',
+  },
 }));
-
-
 
 const Table = () => {
   const classes = useStyles();
   const [state, setState] = useState(initialData);
-  const {loading, error, data} = useQuery(GET_TABLE, {variables: 'Test Table'});
-  const [colUpdate] = useMutation(COL_UPDATE)
+  const { loading, error, data } = useQuery(GET_TABLE, { variables: 'Test Table' });
+  const [colUpdate] = useMutation(COL_UPDATE);
   const [removeTask] = useMutation(REMOVE_TASK);
   const [addTask] = useMutation(ADD_TASK);
-  const [createTask] = useMutation(CREATE_TASK)
+  const [createTask] = useMutation(CREATE_TASK);
 
   const addColumnTask = async (columnId) => {
-    const {data: { addTask } } = await createTask({
+    const {
+      data: { addTask },
+    } = await createTask({
       variables: {
         columnId: columnId,
-        taskContent: `Task created for Column ${columnId}`
-      }
-    })
+        taskContent: `Task created for Column ${columnId}`,
+      },
+    });
 
     const colToUpdate = state.columns[columnId];
-    const updatedTaskIds = [...colToUpdate.taskIds, addTask.id]
+    const updatedTaskIds = [...colToUpdate.taskIds, addTask.id];
     const newColumn = {
       ...colToUpdate,
-      taskIds: updatedTaskIds
-    }
+      taskIds: updatedTaskIds,
+    };
 
     setState({
       ...state,
-      tasks: {...state.tasks, [addTask.id]: addTask },
-      columns: {...state.columns, [addTask.column.id]: newColumn}
-    })
-  }
+      tasks: { ...state.tasks, [addTask.id]: addTask },
+      columns: { ...state.columns, [addTask.column.id]: newColumn },
+    });
+  };
 
   const onDragEnd = async (result) => {
-    const {destination, source, draggableId} = result;
-    const fallbackState = {...state};
+    const { destination, source, draggableId } = result;
+    const fallbackState = { ...state };
 
     if (!destination) {
       return;
     }
 
-    if (
-      destination.droppableId === source &&
-      destination.index === source.index
-    ) {
+    if (destination.droppableId === source && destination.index === source.index) {
       return;
     }
 
     const start = state.columns[source.droppableId];
-    const finish = state.columns[destination.droppableId]
+    const finish = state.columns[destination.droppableId];
 
     if (start === finish) {
-      const newTaskIds = [...start.taskIds]
+      const newTaskIds = [...start.taskIds];
       newTaskIds.splice(source.index, 1);
       newTaskIds.splice(destination.index, 0, draggableId);
 
       const newColumn = {
         ...start,
-        taskIds: newTaskIds
+        taskIds: newTaskIds,
       };
 
       const newState = {
         ...state,
         columns: {
           ...state.columns,
-          [newColumn.id]: newColumn
-        }
+          [newColumn.id]: newColumn,
+        },
       };
 
       setState(newState);
 
-      try{
+      try {
         await colUpdate({
           variables: {
-            ...newColumn
-          }
-        })
+            ...newColumn,
+          },
+        });
         return;
-      } catch(e){
-        setState(fallbackState)
-        console.warn(e)
+      } catch (e) {
+        setState(fallbackState);
+        console.warn(e);
       }
     }
 
-    if(start !== finish){
+    if (start !== finish) {
       const startTaskIds = [...start.taskIds];
       const taskId = startTaskIds[source.index];
       startTaskIds.splice(source.index, 1);
       const newStart = {
         ...start,
-        taskIds: [...start.taskIds.filter(id => id !== draggableId)]
+        taskIds: [...start.taskIds.filter((id) => id !== draggableId)],
       };
 
       const finishTaskIds = [...finish.taskIds];
@@ -119,18 +116,18 @@ const Table = () => {
 
       const newFinish = {
         ...finish,
-        taskIds: finishTaskIds
-      }
+        taskIds: finishTaskIds,
+      };
 
       const newState = {
         ...state,
         columns: {
           ...state.columns,
           [newStart.id]: newStart,
-          [newFinish.id]: newFinish
-        }
-      }
-      setState(newState)
+          [newFinish.id]: newFinish,
+        },
+      };
+      setState(newState);
 
       try {
         const data = await colUpdate({
@@ -150,9 +147,9 @@ const Table = () => {
           },
         });
       } catch (e) {
-        setState(fallbackState)
+        setState(fallbackState);
         console.error(e);
-        console.log('Error at newStart update')
+        console.log('Error at newStart update');
       }
 
       try {
@@ -173,70 +170,63 @@ const Table = () => {
           },
         });
       } catch (e) {
-        setState(fallbackState)
+        setState(fallbackState);
         console.error(e);
-        console.log('Error at newFinish update')
+        console.log('Error at newFinish update');
       }
     }
   };
 
   const setTable = (data) => {
-    const {Table} = data;
+    const { Table } = data;
     const tasks = {};
     const columns = {};
-    const columnOrder = Table[0].columns.map(column => column.id);
+    const columnOrder = Table[0].columns.map((column) => column.id);
     // Pull all tasks out into their own object
     Table[0].columns.forEach((col) => {
       col.tasks.forEach((task) => {
-        tasks[task.id] = {id: task.id, content: task.content}
-      })
+        tasks[task.id] = { id: task.id, content: task.content };
+      });
     });
     // Pull out all columns and their associated task ids
     Table[0].columns.forEach((col) => {
-      columns[col.id] = {id: col.id, title: col.title, taskIds: col.taskIds}
-    })
+      columns[col.id] = { id: col.id, title: col.title, taskIds: col.taskIds };
+    });
 
     const table = {
       tasks,
       columns,
-      columnOrder
-    }
+      columnOrder,
+    };
 
-    console.log(table, ' Table')
+    console.log(table, ' Table');
 
-    setState(table)
-  }
+    setState(table);
+  };
 
   useEffect(() => {
-
     if (data) {
-      setTable(data)
+      setTable(data);
     }
-  }, [data])
+  }, [data]);
 
   if (loading) {
-    return <div>...Loading</div>
+    return <div>...Loading</div>;
   }
 
   if (error) {
-    console.warn(error)
+    console.warn(error);
   }
-
-
 
   return (
     <Container className={classes.root}>
-      <DragDropContext
-        onDragEnd={onDragEnd}
-      >
+      <DragDropContext onDragEnd={onDragEnd}>
         <Paper className={classes.paper}>
-          {
-            state.columnOrder.map((columnId) => {
-              const column = state.columns[columnId];
-              const tasks = column.taskIds.map(taskId => state.tasks[taskId])
-              return <Column key={column.id} column={column} tasks={tasks} addTask={addColumnTask}/>
-            })
-          }
+          {state.columnOrder.map((columnId) => {
+            const column = state.columns[columnId];
+            const tasks = column.taskIds.map((taskId) => state.tasks[taskId]);
+            return <Column key={column.id} column={column} tasks={tasks} addTask={addColumnTask} />;
+          })}
         </Paper>
       </DragDropContext>
     </Container>
