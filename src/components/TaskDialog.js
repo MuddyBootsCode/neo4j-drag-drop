9ab702445b98016124b5263a89b3c09522172fc4
muddybootscode <firsttogo98@gmail.com>
2020-09-28 14:54:00 -0400

    Feature: Dialog added to Deal with editing task

--- src/components/TaskDialog.js
+++ src/components/TaskDialog.js
@@ -10,9 +10,7 @@ import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
-    position: 'absolute',
     width: 'auto',
-    backgroundColor: theme.palette.background.paper,
     border: '2px solid #000',
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
